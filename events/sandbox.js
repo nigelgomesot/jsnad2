3e157603e4c25656a5eb5f3820421313e37a2c16
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-09-01 08:58:00 -0500

    # events WIP...

--- events/sandbox.js
+++ events/sandbox.js
@@ -13,3 +13,55 @@ class MyEmitter extends EventEmitter {
     this.emit('close')
   }
 }
+
+// basic emitter
+const example1 = () => {
+  const ee = new EventEmitter()
+  ee.on('close', () => console.log('close event emitted.'))
+  ee.emit('close')
+}
+
+// basic emitter via addListener
+const example2 = () => {
+  const ee = new EventEmitter()
+  ee.addListener('close', () => console.log('close event emitted.'))
+  ee.emit('close')
+}
+
+// basic with args
+const example3 = () => {
+  const ee = new EventEmitter()
+  ee.on('add', (a, b) => console.log(`sum is ${a + b}`))
+  ee.emit('add', 1, 2)
+}
+
+// ordering
+const example4 = () => {
+  const ee = new EventEmitter()
+  ee.emit('close')
+  ee.on('close', () => console.log('close event emitted'))
+}
+
+// emit order
+const example5 = () => {
+  const ee = new EventEmitter()
+  ee.on('my-event', () => console.log('my-event emitted 1st'))
+  ee.on('my-event', () => console.log('my-event emitted 2nd'))
+  ee.emit('my-event')
+}
+
+// emit order with prependListener
+const example6 = () => {
+  const ee = new EventEmitter()
+  ee.on('my-event', () => console.log('my-event emitted 2nd'))
+  ee.on('my-event', () => console.log('my-event emitted 3rd'))
+  ee.prependListener('my-event', () => console.log('my-event emitted 1st'))
+  ee.prependListener('my-event', () => console.log('my-event emitted 0th'))
+  ee.emit('my-event')
+}
+
+
+const run = () => {
+  example6()
+}
+run()

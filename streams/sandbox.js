ffe0dc43a33efad1044070b2694be51af07b4bfe
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-09-24 13:15:08 -0500

    # streams WIP...

--- streams/sandbox.js
+++ streams/sandbox.js
@@ -29,6 +29,62 @@ const example2 = () => {
   readable.on('end', () => console.log('reading completed'))
 }
 
-const run = () => example2()
+// Readable stream with encoding
+const example3 = () => {
+  const { Readable } = require('stream')
+
+  const createReadableStream = () => {
+    const data = ['A', 'B', 'C', 'D']
+
+    return new Readable({
+      encoding: 'utf8',
+      read() {
+        if (data.length === 0)
+          this.push(null)
+        else
+          this.push(data.shift())
+      }
+    })
+  }
+
+  const readable = createReadableStream()
+  readable.on('data', data => console.log('data received:', data))
+  readable.on('end', () => console.log('reading completed'))
+}
+
+// Readable stream with objectMode
+const example4 = () => {
+  const { Readable } = require('stream')
+
+  const createReadableStream = () => {
+    const data = ['A', 'B', 'C', 'D']
+
+    return new Readable({
+      objectMode: true,
+      read() {
+        if (data.length === 0)
+          this.push(null)
+        else
+          this.push(data.shift())
+      }
+    })
+  }
+
+  const readable = createReadableStream()
+  readable.on('data', data => console.log('data received:', data))
+  readable.on('end', () => console.log('reading completed'))
+}
+
+// Readable.from
+const example5 = () => {
+  const { Readable } = require('stream')
+
+  const readable = Readable.from(['A', 'B', 'C'])
+  readable.on('data', data => console.log('data received:', data))
+  readable.on('end', () => console.log('reading completed'))
+}
+
+
+const run = () => example5()
 run()
 

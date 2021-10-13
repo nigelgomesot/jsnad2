57b583cd2c72787226883ba0fd3e002d9a2f8545
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-10-13 11:00:42 -0500

    # fs WIP...

--- file_system/sandbox.js
+++ file_system/sandbox.js
@@ -302,6 +302,25 @@ const example13 = () => {
   }).listen(3001)
 }
 
-const run = () => example13()
+// metadata
+const example14 = () => {
+  const { readdirSync, statSync } = require('fs')
+  const files =  readdirSync('.')
+
+  for (const name of files) {
+    const stat = statSync(name)
+    const label = stat.isDirectory() ? 'dir': 'file'
+    const { atime, birthtime, ctime, mtime } = stat
+    console.group(label, ':', name)
+    console.log('atime:', atime.toLocaleString())
+    console.log('birthtime:', birthtime.toLocaleString())
+    console.log('ctime:', ctime.toLocaleString())
+    console.log('mtime:', mtime.toLocaleString())
+    console.groupEnd()
+    console.log()
+  }
+}
+
+const run = () => example14()
 run()
 

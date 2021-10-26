93175588b26540fc746a40a0984e0b9795cc6cb4
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-10-26 13:58:12 -0500

    # process_os WIP...

--- process_os/sandbox4.js
+++ process_os/sandbox4.js
@@ -1,9 +1,10 @@
 'use strict'
 
-// os
-const example1 = () => {
-  const os = require('os')
+const os = require('os')
 
+// os basic
+const example1 = () => {
+  
   console.log('os.hostname()', os.hostname())
   console.log('os.homedir()', os.homedir())
   console.log('os.tmpdir()', os.tmpdir())
@@ -11,6 +12,26 @@ const example1 = () => {
   console.log('os.type()', os.type())
 }
 
-const run = () => example1()
+// os stats
+const example2 = () => {
+  let intervalId,
+      count = 0
+
+  const printStats = () => {
+    if (count === 5)
+      return clearInterval(intervalId)
+
+    console.group(count)
+    console.log('os.uptime()', os.uptime())
+    console.log('os.freemem()', os.freemem())
+    console.log('os.totalmem()', os.totalmem())
+    console.groupEnd()
+    count++
+  }
+
+  intervalId = setInterval(printStats, 1000)
+}
+
+const run = () => example2()
 run()
 
